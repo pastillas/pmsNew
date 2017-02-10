886df7f54862c3a3344652c99a23958845839e27
ctorrente <ctorrente@gbox.adnu.edu.ph>
2017-02-10 12:49:05 -0800

    finish

--- pms/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $db = new mysqli('localhost', 'root', '', 'z');
-
-  if($db->connect_errno > 0){
-    die('Unable to connect to database [' . $db->connect_error . ']');
-  }
-?>
--- sampleItems.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO  ITEMS VALUES('item1', 'Cement', 'bag', '', 230);
-INSERT INTO  ITEMS VALUES('item2', 'Flat Latex Paint', 'liter', '', 500);
-INSERT INTO  ITEMS VALUES('item3', 'Gloss Paint', 'liter', '', 590);
-INSERT INTO  ITEMS VALUES('item4', 'Flatwall Enamel Paint', 'liter', '', 500);
-INSERT INTO  ITEMS VALUES('item5', 'QDE Paint', 'liter', '', 600);
-INSERT INTO  ITEMS VALUES('item6', '4" Hollow Blocks', 'inch', '', 14);
-INSERT INTO  ITEMS VALUES('item7', '6" Hollow Blocks', 'inch', '', 17);
